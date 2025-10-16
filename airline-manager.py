74822d05eed72005a9147b8e361c01c789d85fda
BMan-21 <bodhibhatia@gmail.com>
2025-10-16 10:31:11 -0700

    Add files via upload

--- airline-manager.py
+++ airline-manager.py
@@ -1,1 +1,24 @@
+from time import sleep
+from random import randint
+print("welcome to airline manager!")
+hangar = []
+routes = ["KLAX <> KSFO (337.4 mi)"]
+flights = 
+def flightsetup (plane)
+    flightnum = input("what 3 digit flight number would you like?" )
+    if len(int(flightnum)) != 3:
+        print("inavlid flight number!")
+    else:
+       print(routes)
+       route = input("which route would you like? (type number) ")
+    
+name = input("what would you like to name your airline? ")
+abbrev = input("what 2 letter abbbreviation would you like for your airline? (ex. EA for Expedition Airways): ")
+if len(abbrev) != 2:
+    print("the abbreviation must be 2 letters!")
+    exit()
+print("your first plane will be a DHC-6 Twin Otter!")
+hangar += "DHC-6"
+print("let's set up your first flight!")
+
 
